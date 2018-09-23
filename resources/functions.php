66631739457e25deefdf9579876276a95cfb43a8
UFWestlife <uflzun93@gmail.com>
2018-09-22 23:16:54 -0700

    Modified static page layouts using heredoc and nowdoc.

--- resources/functions.php
+++ resources/functions.php
@@ -1,16 +1,19 @@
 <?php 
-function redrict(){
+
+// helper functions
+
+function redrict($location){
 	header("Location: $location ");
 }
 
-function query(){
+function query($sql){
 	global $connection; 
 	return mysqli_query($connection, $sql);
 }
 
 function confirm($result){
 	global $connection;
-	if (!result){
+	if (!$result){
 		die("QUERY FAILED " . mysqli_error($connection));
 	}
 }
@@ -25,4 +28,39 @@ function fetch_array($result){
 	return mysqli_fetch_array($result);
 }
 
- ?>
+//get products
+
+function get_products() {
+$query = query("SELECT * FROM products");
+confirm($query);
+
+while($row = fetch_array($query)) {
+// echo $row['product_price'];
+$product = <<<DELIMETER
+<div class="col-sm-4 col-lg-4 col-md-4">
+<div class="thumbnail">
+    <img src="http://placehold.it/320x150" alt="">
+    <div class="caption">
+        <h4 class="pull-right">{$row['product_price']}</h4>
+        <h4><a href="product.html">First Product</a>
+        </h4>
+        <p>See more snippets like this online store item at <a target="_blank" href="http://www.bootsnipp.com">Bootsnipp - http://bootsnipp.com</a>.</p>
+    <a class="btn btn-primary" target="_blank" href="http://maxoffsky.com/code-blog/laravel-shop-tutorial-1-building-a-review-system/">View Tutorial</a>
+    </div>
+
+</div>
+</div>
+DELIMETER;
+
+echo $product;
+
+
+}
+
+
+
+
+
+}
+
+?>
